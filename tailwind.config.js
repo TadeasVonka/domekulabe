68ddcf5938e9e1af4530b3748b12e5f5ea4024ae
TadeasVonka <vonkat22@spstrutnov.cz>
2024-04-02 19:29:41 +0200

    editted

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,9 @@ module.exports = {
     extend: {
       colors: {
         beige: "#9E7E44",
-        gainsboro: "#F1EFF2",
       },
       width: {
         13: "3.25rem",
-        30: "30px",
       },
       maxWidth: {
         1440: "1440px",
@@ -19,7 +17,6 @@ module.exports = {
       },
       height: {
         504: "504px",
-        30: "30px",
       },
       fontSize: {
         xxs: "0.625rem",
@@ -27,6 +24,7 @@ module.exports = {
       },
       fontFamily: {
         Raleway: ["Raleway", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
       },
       backgroundImage: {
         domek: "url('obrazky/barakk.png')",
